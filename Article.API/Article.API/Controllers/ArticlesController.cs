bbabaedec3ed9e80a354c5e677e17726ed838d64
Ahmet Ayaz <ayazahmet34@gmail.com>
2019-10-21 10:36:10 +0300

    Implemented http methods

--- Article.API/Article.API/Controllers/ArticlesController.cs
+++ Article.API/Article.API/Controllers/ArticlesController.cs
@@ -10,36 +10,77 @@ namespace Article.API.Controllers
     [ApiController]
     public class ArticlesController : ControllerBase
     {
+        readonly ArticleContext context;
+
+        public ArticlesController(ArticleContext context)
+        {
+            this.context = context;
+        }
+
         // GET api/articles
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public IEnumerable<Article> Get()
         {
-            return new string[] { "value1", "value2" };
+            return context.Article.ToList();
         }
 
         // GET api/articles/5
         [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        public ActionResult Get(int id)
         {
-            return "value";
+            var article = context.Article.FirstOrDefault(x => x.Id == id);
+            if (article == null)
+            {
+                return NotFound();
+            }
+
+            return new ObjectResult(article);
         }
 
         // POST api/articles
         [HttpPost]
-        public void Post([FromBody] string value)
+        public ActionResult Post([FromBody] Article article)
         {
+            context.Article.Add(article);
+            context.SaveChanges();
+
+            return Ok();
         }
 
         // PUT api/articles/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public ActionResult Put([FromBody] Article article)
         {
+            var existingArticle = context.Article.FirstOrDefault(x => x.Id == article.Id);
+            if (existingArticle == null)
+            {
+                return NotFound();
+            }
+
+            existingArticle.Name = article.Name;
+            existingArticle.Subject = article.Subject;
+            existingArticle.Author = article.Author;
+            existingArticle.PublishDate = article.PublishDate;
+
+            context.SaveChanges();
+
+            return Ok();
         }
 
         // DELETE api/articles/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public ActionResult Delete(int id)
         {
+            var willBeDeletedArticle = context.Article.FirstOrDefault(x => x.Id == id);
+            if (willBeDeletedArticle == null)
+            {
+                return BadRequest("Not valid a article. Article Id: " + id);
+            }
+
+            context.Article.Remove(willBeDeletedArticle);
+            context.SaveChanges();
+
+            return Ok();
         }
     }
 }
