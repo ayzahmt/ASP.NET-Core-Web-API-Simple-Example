c0f937ca0f3f91e1d71232248e9c9d5358360a9b
Ahmet Ayaz <ayazahmet34@gmail.com>
2019-10-21 10:02:06 +0300

    Added Models file (Contract and Context)

---
+++ Article.API/Article.API/Models/Article.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Article.API.Models
+{
+    public class Article
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public string Subject { get; set; }
+        public DateTime PublishDate { get; set; }
+    }
+}
