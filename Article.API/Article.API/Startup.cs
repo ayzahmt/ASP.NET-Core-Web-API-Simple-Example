df0806794215c4c3eb674c6d1c35e97842311e13
Ahmet Ayaz <ayazahmet34@gmail.com>
2019-10-21 10:57:58 +0300

    Added LogProvider to Configure

--- Article.API/Article.API/Startup.cs
+++ Article.API/Article.API/Startup.cs
@@ -31,8 +31,10 @@ namespace Article.API
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            loggerFactory.AddProvider(new LogProvider());
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
