9b73c215117ca4ac5122a3d07d6d438e9b892e2b
Ahmet Ayaz <ayazahmet34@gmail.com>
2019-10-21 09:58:00 +0300

    Rename Controller class

--- Article.API/Article.API/Controllers/ArticlesController.cs
+++ Article.API/Article.API/Controllers/ArticlesController.cs
@@ -8,35 +8,35 @@ namespace Article.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
+    public class ArticlesController : ControllerBase
     {
-        // GET api/values
+        // GET api/articles
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/values/5
+        // GET api/articles/5
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
             return "value";
         }
 
-        // POST api/values
+        // POST api/articles
         [HttpPost]
         public void Post([FromBody] string value)
         {
         }
 
-        // PUT api/values/5
+        // PUT api/articles/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
         }
 
-        // DELETE api/values/5
+        // DELETE api/articles/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
