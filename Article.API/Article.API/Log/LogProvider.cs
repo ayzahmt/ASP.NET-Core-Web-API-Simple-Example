841bea2aa6079ae5f9cd7da1cc14d39f12746950
Ahmet Ayaz <ayazahmet34@gmail.com>
2019-10-21 10:52:33 +0300

    Added log files

---
+++ Article.API/Article.API/Log/LogProvider.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Article.API.Log
+{
+    public class LogProvider : ILoggerProvider
+    {
+        public ILogger CreateLogger(string categoryName)
+        {
+            return new Logger();
+        }
+
+        public void Dispose()
+        {
+
+        }
+    }
+}
---
+++ Article.API/Article.API/Log/Logger.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Article.API.Log
+{
+    public class Logger : ILogger
+    {
+        public IDisposable BeginScope<TState>(TState state)
+        {
+            return null;
+        }
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            return true;
+        }
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            var message = string.Format("{0}: {1} - {2}", logLevel.ToString(), eventId.Name, formatter(state, exception));
+            WriteToFile(message);
+        }
+
+        void WriteToFile(string message)
+        {
+            var path = Path.GetDirectoryName(Environment.CurrentDirectory);
+            var fullPath = path + "\\Log.txt";
+            using (var sw = new StreamWriter(fullPath, true))
+            {
+                sw.WriteLine(message);
+                sw.Close();
+            }
+        }
+    }
+}
